1c1da55f1cce297bcd3686d8439d16bb73bcecab
Erkut Can Aksoy <erkutcan.aksoy@asml.com>
2024-01-12 16:38:41 +0100

    Insertion sort initial commit

---
+++ SortingAlgorithms/InsertionSort.py
@@ -0,0 +1,9 @@
+import random
+
+size = 10
+min_value = 0
+max_value = 100
+random_ints = [ random.randint(min_value, max_value) for i in range(size)]
+
+def insertion_sort(ints_to_be_sorted):
+    pass
